dade997f7543034391d1586e22107b8b2eab85c0
Garwel <garwel@protonmail.com>
2022-10-26 09:35:01 +0200

    Added JobDriver, WorkGiver classes

---
+++ Source/JobDriver_RecruitSlave.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse.AI;
+
+namespace RecruitSlaves
+{
+    public class JobDriver_RecruitSlave : JobDriver
+    {
+        public override bool TryMakePreToilReservations(bool errorOnFailed)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IEnumerable<Toil> MakeNewToils()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Source/WorkGiver_RecruitSlave.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using Verse.AI;
+
+namespace RecruitSlaves
+{
+    public class WorkGiver_RecruitSlave : WorkGiver_Warden
+    {
+        public override Job JobOnThing(Pawn pawn, Thing t, bool forced = false)
+        {
+            if (!ModLister.IdeologyInstalled)
+                return null;
+            if (!ShouldTakeCareOfSlave(pawn, t))
+                return null;
+            Pawn target = t as Pawn;
+            return null;
+        }
+    }
+}

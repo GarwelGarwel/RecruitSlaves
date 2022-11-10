a3ea9b9ad19b1fc1dde3b7746e77f65d0f8db25f
Garwel <garwel@protonmail.com>
2022-11-10 11:15:15 +0100

    Replaced population StatPart with population intent; rebalanced other stat factors

---
+++ Source/Stats/StatPart_PopulationIntent.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace RecruitSlaves
+{
+    public class StatPart_PopulationIntent : StatPart_Curve
+    {
+        protected override bool AppliesTo(StatRequest req) => true;
+
+        protected override float CurveXGetter(StatRequest req) => StorytellerUtilityPopulation.PopulationIntent;//PawnsFinder.AllMapsCaravansAndTravelingTransportPods_Alive_Colonists_NoSlaves.Count;
+
+        protected override string ExplanationLabel(StatRequest req) => $"Storyteller intent ({CurveXGetter(req).ToStringWithSign()})";
+    }
+}

27fc5220ff31f8acb10f6d012c23db6d308700fc
Garwel <garwel@protonmail.com>
2023-01-09 18:51:25 +0100

    Added Time As Colony Slave record

---
+++ Source/RecordWorker_TimeAsSlave.cs
@@ -0,0 +1,10 @@
+﻿using RimWorld;
+using Verse;
+
+namespace RecruitSlaves
+{
+    public class RecordWorker_TimeAsColonySlave : RecordWorker
+    {
+        public override bool ShouldMeasureTimeNow(Pawn pawn) => pawn.IsSlaveOfColony;
+    }
+}

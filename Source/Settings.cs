8405fbf31026d9663e3f79ee6fd5db924a33b4c2
GarwelGarwel <garwel@protonmail.com>
2024-04-23 23:39:02 +0200

    Keeping min suppression is now on by default at 70%

--- Source/Settings.cs
+++ Source/Settings.cs
@@ -14,6 +14,7 @@ namespace RecruitSlaves
 
         public const float RecruitmentDifficultyMultiplier_Base = 10;
         public const int RecruitmentAttemptCooldown_Default = 24;
+        public const float KeepMinSuppression_Default = 0.70f;
 
         public static int RecruitmentAttemptCooldownTicks => RecruitmentAttemptCooldown * GenDate.TicksPerHour;
 
@@ -24,7 +25,7 @@ namespace RecruitSlaves
             base.ExposeData();
             Scribe_Values.Look(ref RecruitmentDifficulty, "RecruitmentDifficulty", 1);
             Scribe_Values.Look(ref RecruitmentAttemptCooldown, "RecruitmentAttemptCooldown", RecruitmentAttemptCooldown_Default);
-            Scribe_Values.Look(ref KeepMinSuppression, "KeepMinSuppression");
+            Scribe_Values.Look(ref KeepMinSuppression, "KeepMinSuppression", KeepMinSuppression_Default);
             Scribe_Values.Look(ref DebugLogging, "DebugLogging");
             if (Scribe.mode == LoadSaveMode.LoadingVars)
                 Print();
@@ -35,7 +36,7 @@ namespace RecruitSlaves
             Log($"Resetting settings.");
             RecruitmentDifficulty = 1;
             RecruitmentAttemptCooldown = RecruitmentAttemptCooldown_Default;
-            KeepMinSuppression = 0;
+            KeepMinSuppression = KeepMinSuppression_Default;
             Print();
         }
 
@@ -43,9 +44,9 @@ namespace RecruitSlaves
         {
             if (!DebugLogging)
                 return;
-            Log($"RecruitmentDifficulty: {RecruitmentDifficulty}");
-            Log($"RecruitmentAttemptCooldown: {RecruitmentAttemptCooldown}");
-            Log($"KeepMinSuppression: {KeepMinSuppression}");
+            Log($"RecruitmentDifficulty: {RecruitmentDifficulty.ToStringPercent()}");
+            Log($"RecruitmentAttemptCooldown: {RecruitmentAttemptCooldown.ToStringCached()} h");
+            Log($"KeepMinSuppression: {KeepMinSuppression.ToStringPercent()}");
         }
     }
 }
